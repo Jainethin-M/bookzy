d77e19abdff7eb521d1ed4e212fd34163fddc62b
Jainethin <jainethin59@gmail.com>
2025-07-18 12:20:48 +0530

    done

--- frontend/src/constant/url.js
+++ frontend/src/constant/url.js
@@ -1,3 +1,3 @@
 // export const baseURL = process.env.NODE_ENV === 'production'?'https://petshop-wy6i.onrender.com':'http://localhost:5000'
-export const baseURL = 'http://localhost:5000'
+export const baseURL = 'https://bookzy-izbp.onrender.com'
 export const books = 'https://www.googleapis.com/books/v1/volumes?q=*'
